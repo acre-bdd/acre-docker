bec40a8c9e0ebe8be8b2b7cde28df104ebf4c82a
Claudio Klingler <ck@realtime-projects.com>
2022-11-17 21:13:45 +0100

    create user 'tester' with correct user id (of host user).

--- acre/docker/base/installer/tester-user.sh
+++ acre/docker/base/installer/tester-user.sh
@@ -1,3 +1,6 @@
-adduser --disabled-password --gecos '' tester
+[ -z $1 ] || export USER_ID=$1
+USER_ID=${USER_ID:-9001}
+echo USER_ID is $USER_ID
+adduser --uid $USER_ID --disabled-password --gecos '' tester
 adduser tester sudo
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
---
+++ releases.md
@@ -0,0 +1,9 @@
+
+# v0.1.0
+
+-   tester user is created with the host's user id to prevent
+    permission errors.
+
+# v0.0.1
+
+-   initial release
