fdd8b4bf50c9c1899836bb2186678b6c60ed64c1
Claudio Klingler <ck@realtime-projects.com>
2022-12-01 18:05:50 +0100

    make "tester" user a sudoer.

--- acre/docker/base/installer/tester-user.sh
+++ acre/docker/base/installer/tester-user.sh
@@ -2,5 +2,6 @@
 USER_ID=${USER_ID:-9001}
 echo USER_ID is $USER_ID
 adduser --uid $USER_ID --disabled-password --gecos '' tester
+addgroup sudo
 adduser tester sudo
-echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+echo '%sudo ALL=(ALL) NOPASSWD: ALL' >/etc/sudoers
